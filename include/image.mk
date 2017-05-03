f9ecf405172e07f170cdf54717f2637d199177d7
Jan Pavlinec <jan.pavlinec@nic.cz>
2017-05-03 13:02:10 +0200

    compile_fw: replace mv with cp

--- include/image.mk
+++ include/image.mk
@@ -270,7 +270,9 @@ define Image/Checksum
 		$(FIND) -maxdepth 1 -type f \! -name 'md5sums'  -printf "%P\n" > sumslist.txt ; \
 		sort sumslist.txt>sumlist_sorted.txt ; \
 		echo "$1">aaaaaaaaaa.txt ; \
+		pwd >> aaaaaaaaaa.txt ; \
 		echo "$2">bbbbbbbbbb.txt ; \
+		pwd >> bbbbbbbbbb.txt ; \
 		cat sumlist_sorted.txt | xargs $1 > $2 ; \
 	)
 endef
