a44a1f7d341788ba3f8f95c6011a1ea1ea24a166
Jan Pavlinec <jan.pavlinec@nic.cz>
2017-05-03 17:19:24 +0200

    image.mk: disable exit on error

--- include/image.mk
+++ include/image.mk
@@ -268,12 +268,14 @@ define Image/Checksum
 
 	( cd ${BIN_DIR} ; \
 		$(FIND) -maxdepth 1 -type f \! -name 'md5sums'  -printf "%P\n" > sumslist.txt ; \
+		set +e ; \
 		sort sumslist.txt>sumlist_sorted.txt ; \
 		echo "$1">aaaaaaaaaa.txt ; \
 		pwd >> aaaaaaaaaa.txt ; \
 		echo "$2">bbbbbbbbbb.txt ; \
 		pwd >> bbbbbbbbbb.txt ; \
 		cat sumlist_sorted.txt | xargs $1 > $2 ; \
+		set -e ; \
 	)
 endef
 
